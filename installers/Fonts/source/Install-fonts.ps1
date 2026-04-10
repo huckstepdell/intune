f333b05173604acedf50013106e1e4be03d88b49
Colin Huckstep <31158953+colinhuckstep@users.noreply.github.com>
2026-04-10 11:42:57 -0400

    Update Install-fonts.ps1

--- installers/Fonts/source/Install-fonts.ps1
+++ installers/Fonts/source/Install-fonts.ps1
@@ -1,1 +1,20 @@
-placeholder so git will track the empty folder
+$fontsPath = "$env:WINDIR\Fonts"
+$regPath  = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts"
+
+$fontFiles = @(
+    @{ File = "MyFont-Regular.ttf"; Name = "My Font (TrueType)" },
+    @{ File = "MyFont-Bold.ttf";    Name = "My Font Bold (TrueType)" }
+)
+
+foreach ($font in $fontFiles) {
+    $source = Join-Path $PSScriptRoot $font.File
+    $dest   = Join-Path $fontsPath   $font.File
+
+    if (-not (Test-Path $dest)) {
+        Copy-Item $source $dest -Force
+    }
+
+    New-ItemProperty -Path $regPath -Name $font.Name -Value $font.File -PropertyType String -Force | Out-Null
+}
+
+exit 0
